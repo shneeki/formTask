7b1d14329b0c8d46956cb65b15872e1e16641d50
Maxat Nukhayev <maxat.nukhayev@gmail.com>
2022-06-22 15:36:38 +0200

    added theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { SnackbarProvider } from 'notistack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f0a04b',
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
