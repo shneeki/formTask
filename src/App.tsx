c4ed51c5de8fa4ce16e6a9f9626520181d8058f2
Maxat Nukhayev <maxat.nukhayev@gmail.com>
2022-06-22 11:05:48 +0200

    basic MUI form, no interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useForm } from 'react-hook-form';
+
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function App() {
-  return <h2>Hi</h2>;
+  const [value, setValue] = React.useState<Date | null>(null);
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh">
+      <form className="signupForm">
+        <div className="formInput">
+          <TextField
+            required
+            sx={{
+              width: '100%',
+            }}
+            id="outlined-required"
+            label="Username"
+            defaultValue=""
+          />
+        </div>
+        <div className="formInput">
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <DatePicker
+              label="Birthday"
+              value={value}
+              onChange={(newValue: Date | null) => {
+                setValue(newValue);
+              }}
+              renderInput={(params: any) => <TextField {...params} fullWidth />}
+            />
+          </LocalizationProvider>
+        </div>
+
+        <div className="formInput">
+          <TextField
+            sx={{
+              width: '100%',
+            }}
+            id="outlined"
+            label="Age"
+            defaultValue=""
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </div>
+        <FormControlLabel
+          control={<Checkbox size="medium" />}
+          label="I have read and agree to User Agreement and Privacy Policy."
+        />
+
+        <Button variant="contained">Register</Button>
+      </form>
+    </Box>
+  );
 }
 
 export default App;
